314e4cd3dbdf81dd9dc7c926d0d2be774f7679ce
Lesona-Systems <nejwritings@gmail.com>
2022-06-28 08:34:42 -0500

    add function to print current weather for specified city

--- weather.py
+++ weather.py
@@ -3,7 +3,7 @@ from requests.exceptions import HTTPError
 from configparser import ConfigParser
 
 
-
+# parse secrets.ini for OpenWeather API key
 def _get_api_key():
   config = ConfigParser()
   config.read('secrets.ini')
@@ -13,11 +13,11 @@ def _get_api_key():
 api_key = _get_api_key()
 
 print('Get your local weather!')
-
 user_zip = input('Enter your zip code: ')
 
 # Form Geocoding API url
 geo_url = f"http://api.openweathermap.org/geo/1.0/zip?zip={user_zip},US&appid={api_key}"
+
 # get and parse zip into lat/long for use in current weather API call
 try:
   get_latlon = requests.get(geo_url)
@@ -42,11 +42,20 @@ weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={l
 try:
   get_current_weather = requests.get(weather_url)
   get_current_weather.raise_for_status()
-  weather = get_current_weather.json()
+  weather_data = get_current_weather.json()
 
 except HTTPError as http_err:
   print(f'HTTP error occuperd: {http_err}')
 except Exception as err:
   print(f'Other error occured: {err}')
 
-print(weather)
+# assign city, weather description, and temperature to values in returned weather data dict
+
+def display_weather_info():
+  city = weather_data['name']
+  weather_description = weather_data['weather'][0]['description']
+  temperature = weather_data['main']['temp']
+
+  print(f'In {city}, the weather is currently {weather_description} with a temperature of ({temperature}°F).')
+
+display_weather_info()
